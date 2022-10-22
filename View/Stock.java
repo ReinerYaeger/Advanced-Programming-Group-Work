907f1494f05041b10d869ccb4c34ad9e11b79378
g-a-archer <79874798+g-a-archer@users.noreply.github.com>
2022-10-22 12:29:11 -0500

    Update Stock.java

--- View/Stock.java
+++ View/Stock.java
@@ -1,141 +1,108 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.Border;
+import javax.swing.table.DefaultTableModel;
 
-
-public class SalesReport {
+public class Stock{
 
 	private JFrame frame;
-	private JLabel navigationLabel;
-	private GridBagConstraints gbc;
-	private JComboBox<String> combobox;
 	private JButton button;
-	private JLabel label;
-	private JLabel dateLabel;
-	private JLabel fromLabel;
-	private JLabel toLabel;
-	private JTextField fromTextField;
-	private JTextField toTextField;
-	private JButton printButton;
-	SalesReport(){
+	private JLabel navigationLabel;
+	private JComboBox<String> combobox;
+	private JLabel headerLabel;
+	private JTable table;
+	private JPanel panel1;
+	private JPanel panel2;
+	private JPanel panel3;
+	private JPanel panel4;
+	private JPanel panel5;
+	private JPanel panel6;
+	private JPanel borderPanel;
+	private String headings[]= {"Product Code","Name","Short Description","Long Description","Items in Stock","Unit Price"};
+	private int row=15;
+	private JButton updateButton;
+	private JButton deleteButton;
+	private JButton insertButton;
+	 
+	public Stock(){
 		frame=new JFrame();
-		navigationLabel=new JLabel("Navigation:");
-		frame.add(navigationLabel);
-		
-		button=new JButton("Logout");
-		String navigation[]= {" ","Dashboard","Customer Database","Staff Database","Stock and Inventory","Check Out","Sales Reports","Register Customer"};
+		 
+		 navigationLabel=new JLabel("Navigation:");
+		 button=new JButton("Logout");
+		 String navigation[]= {" ","Dashboard","Customer Database","Staff Database","Stock and Inventory","Check Out","Sales Reports","Register Customer"};
 		 combobox=new JComboBox<>(navigation);
-		//combobox.setBounds(100, 50, 0, 0);
-		combobox.setSelectedIndex(0);
-		frame.add(combobox);
-		label=new JLabel("Sale Reports");
-		dateLabel=new JLabel("Date mm/dd/yyyy");
-		fromLabel=new JLabel("From:");
-		fromTextField=new JTextField();
-		toLabel=new JLabel("To");
-		toTextField=new JTextField();
-		printButton=new JButton("Print");
+		 headerLabel=new JLabel("Stock and Inventory");
+		 DefaultTableModel model = new DefaultTableModel(row, headings.length) ;
+		 model.setColumnIdentifiers(headings);
+		 table=new JTable(model);
+		Border border= BorderFactory.createLineBorder(Color.black);
+		 updateButton=new JButton("Update");
+		 deleteButton=new JButton("Delete");
+		 insertButton=new JButton("Insert");
+		 panel1=new JPanel(new FlowLayout(FlowLayout.LEFT));
+		 panel2=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		 panel3=new JPanel(new FlowLayout(FlowLayout.CENTER));
+		 panel4=new JPanel(new BorderLayout(0,0));
+		 panel5=new JPanel(new BorderLayout(0,0));
+		 panel5.setSize(300,400);
+		 panel6=new JPanel(new FlowLayout());
+		 borderPanel=new JPanel();
+		 borderPanel.setBorder(border);
+		
 		layout();
 	}
-	public void layout() {
-		frame.setLayout(new GridBagLayout());
-		//navigationLabel.setSize(150,20);
 	
-		gbc=new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.gridwidth = 1; 
-		gbc.ipadx = 50;
-		gbc.ipady = 20;
-		gbc.anchor = GridBagConstraints.NORTHWEST;
-        frame.add(navigationLabel,gbc);
-        
-        gbc=new GridBagConstraints();
-		gbc.gridx = 1;
-		gbc.gridy = 0;
-		gbc.gridwidth = 1; 
-		gbc.ipadx = 40;
-		gbc.ipady = 5;
-		gbc.anchor = GridBagConstraints.NORTHWEST;
-        frame.add(combobox,gbc);
-        
-		gbc = new GridBagConstraints();
-		gbc.gridx = 3;
-		gbc.gridy = 0;
-		gbc.gridwidth = 1;
-		gbc.ipadx = 20;
-		gbc.ipady = 10;
-		gbc.anchor = GridBagConstraints.EAST;
-		frame.add(button,gbc);
+	public void layout() {
+		frame.setLayout(new GridLayout(0,1,1,2));
+		panel1.add(navigationLabel);
+		panel1.add(combobox);
+		//frame.add(panel1);
+		panel2.add(button);
+		//frame.add(panel2);
+		panel4.add(panel1,BorderLayout.WEST);
+		panel4.add(panel2,BorderLayout.EAST);
+		frame.add(panel4);
+		panel3.add(headerLabel);
 		
-		gbc = new GridBagConstraints();
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		gbc.gridwidth = 1;
-		gbc.anchor = GridBagConstraints.CENTER;
+		frame.add(panel3);
+		panel5.add(table.getTableHeader(),BorderLayout.NORTH);
+		panel5.add(table);
+		frame.add(panel5);
+		panel6.add(updateButton);
+		panel6.add(deleteButton);
+		panel6.add(insertButton);
+		frame.add(panel6);
 		
-		frame.add(label,gbc);
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		gbc.gridwidth = 1;
-		gbc.anchor = GridBagConstraints.WEST;
-		frame.add(dateLabel,gbc);
+		borderPanel.add(panel5);
+		borderPanel.add(panel6);
 		
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 4;
-		gbc.gridwidth = 1;
-		gbc.anchor = GridBagConstraints.WEST;
-		frame.add(fromLabel,gbc);
-		gbc = new GridBagConstraints();
-		gbc.gridx = 1;
-		gbc.gridy = 4;
-		gbc.gridwidth = 1;
-		gbc.ipadx = 90;
-		gbc.ipady = 5;
-		gbc.anchor = GridBagConstraints.WEST;
-		frame.add(fromTextField,gbc);
-
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 8;
-		gbc.gridwidth = 1;
-		gbc.anchor = GridBagConstraints.WEST;
-		frame.add(toLabel,gbc);
-		gbc = new GridBagConstraints();
-		gbc.gridx = 1;
-		gbc.gridy = 8;
-		gbc.gridwidth = 1;
-		gbc.ipadx = 90;
-		gbc.ipady = 5;
-		gbc.anchor = GridBagConstraints.WEST;
-		frame.add(toTextField,gbc);
-		
-		gbc = new GridBagConstraints();
-		gbc.gridx = 2;
-		gbc.gridy = 8;
-		gbc.gridwidth = 1;
-		gbc.ipadx = 20;
-		gbc.ipady = 10;
-		gbc.anchor = GridBagConstraints.WEST;
-		frame.add(printButton,gbc);
-		//ADD EVERYTHING TO A PANEL AND THEN ADD THE BORDER
-		//frame.getContentPane().add(toTextField, BorderLayout.CENTER);
-		frame.setSize(600,200);
+		//borderPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		frame.getContentPane().add(borderPanel,BorderLayout.CENTER);
+		frame.setSize(new Dimension(400,600));
 		frame.setVisible(true);
-		frame.isResizable();
+		frame.setResizable(true);
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	public static void main(String[] args) {
-		new SalesReport();
-
+		new Stock();
 	}
+	
+	
 
+	
 }
