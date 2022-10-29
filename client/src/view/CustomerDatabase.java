f89a9fcecd43696f17f1ee5cb6c9bdf23efee47b
Seantae-Laylor <100803770+Seantae-Laylor@users.noreply.github.com>
2022-10-29 14:52:06 -0500

    Update CustomerDatabase.java

--- client/src/view/CustomerDatabase.java
+++ client/src/view/CustomerDatabase.java
@@ -1,130 +1,159 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.TextField;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
 
-import controller.Controller;
-import model.Customer;
+public class CustomerDatabase extends JPanel{
 
-@SuppressWarnings("serial")
-public class CustomerDatabase extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	//Create variables
 	private JFrame frame;
-	private JLabel customerDatabaseLabel, searchLabel;
+	private JLabel customerDatabaseLabel,searchLabel,navigationLabel;
 	private TextField searchTextField;
-	private JButton logoutButton;
-	private JButton navigationButton;
-	private JPanel buttons;
-	private DefaultTableModel tableItems;
-	private JPanel middle;
-	private JPanel table1;
+	private JButton logoutButton;	
+	private JPanel searchTextFieldPanel;
+	private JPanel searchLabelPanel;
+	private JPanel tablePanel;
+	private JPanel navigationPanel;
+	private JPanel logoutPanel;
+	private JPanel customerDatabasePanel;
+	private JPanel panel1;
+	private JPanel panel2;
 	private JTable table;
-	private JScrollPane jsp;
+	private JScrollPane scrollPane;
+	private JComboBox<String> combobox;
 	
 	private List<Customer> allCustomers; 
 
+
 	public CustomerDatabase() {
-		frame = new JFrame("Customer View");
+		//Initialize the variables
+		frame = new JFrame("Customer Database");
+		navigationPanel = new JPanel();
+		logoutPanel = new JPanel();
+		customerDatabasePanel = new JPanel();
+		searchTextFieldPanel = new JPanel();
+		searchLabelPanel = new JPanel();
 		logoutButton = new JButton("Logout");
-		navigationButton = new JButton("Navigation");
+		navigationLabel = new JLabel("Navigation");
 		customerDatabaseLabel = new JLabel("Customer Database");
-		searchTextField = new TextField();
+		searchTextField = new TextField(40);
 		searchLabel = new JLabel("Search");
+		panel1 = new JPanel(new BorderLayout(0,0));
+		panel2 = new JPanel((LayoutManager) new FlowLayout(FlowLayout.RIGHT));
+		tablePanel = new JPanel();
 
-		table1 = new JPanel(new GridLayout(1, 1));
-		buttons = new JPanel(new GridLayout(1, 3));
-		middle = new JPanel(new GridLayout(1, 1));
+		//Create Array of string
+		String navigation[]= {" ","Dashboard","Customer Database","Staff Database","Stock and Inventory","Check Out","Sales Reports","Register Customer"};
+		combobox=new JComboBox<>(navigation);
+		//Set position on the frame
+		combobox.setBounds(100, 50, 150, 20);
 
-		String[] columns = { "Customer ID", "Name", "DOB", "Address", "Telephone", "Email", "Date of Membership",
-				"Date of Membership Expiry" };
+		//Create Array of string
+	String columns[] = {"Customer ID", "Name", "DOB", "Address","Telephone","Email","Date of Membership","Date of Membership Expiry"};
+	int rows = 15;
 
-		tableItems = new DefaultTableModel(columns, 0);
+	DefaultTableModel model = new DefaultTableModel(rows, columns.length) ;
+	model.setColumnIdentifiers(columns);
+	table=new JTable(model);
 
-		table = new JTable(tableItems) {
-			@Override
-			public boolean isCellEditable(int data_row, int columns) {
-				return false;
-			}
+	//Set size of viewport for the table
+	table.setPreferredScrollableViewportSize(new Dimension(1200,100));
+	table.setFillsViewportHeight(true);
+	//Add table to scrollpane view
+     scrollPane = new JScrollPane(table);
 
-			@Override
-			public Component prepareRenderer(TableCellRenderer renderer, int dataRows, int columns) {
-				Component row = super.prepareRenderer(renderer, dataRows, columns);
-				if (dataRows % 2 == 0) {
-					row.setBackground(Color.WHITE);
-				} else {
-					row.setBackground(Color.LIGHT_GRAY);
-				}
-				if (isCellSelected(dataRows, columns)) {
-					row.setBackground(Color.GREEN);
-				}
-				return row;
-			}
-		};
-		table.setPreferredScrollableViewportSize(new Dimension(1300, 500));
-		table.setFillsViewportHeight(true);
-		jsp = new JScrollPane(table);
-		add(jsp);
-
-		layoutComponents();
-		loadCustomerData();
+	 //Call layoutComponents method
+	layoutComponents();
+	//call loadCustomerData method
+	loadCustomerData();
 	}
 
+	
 	private void layoutComponents() {
-		frame.setLayout(new GridLayout(3, 3, 0, 20));
+		//Set the Layout Manager for the frame
+		frame.setLayout(new GridLayout(0,1,0,0));
 
-		buttons.add(navigationButton);
-		buttons.add(customerDatabaseLabel);
-		buttons.add(logoutButton);
-		frame.add(buttons);
+		//Add label to panel
+		customerDatabasePanel.add(customerDatabaseLabel);
+		//Add panel to frame
+		frame.add(customerDatabasePanel);
 
-		middle.add(searchLabel);
-		middle.add(searchTextField);
-		frame.add(middle);
+		//Add label to panel
+		navigationPanel.add(navigationLabel);
+		//Add combobox to panel
+		navigationPanel.add(combobox);
+		//Add button to panel
+		logoutPanel.add(logoutButton);
+		//Add panel to panel using borderlayout
+		panel1.add(navigationPanel,BorderLayout.WEST);
+		panel1.add(logoutPanel,BorderLayout.EAST);
+		//Add panel to frame
+		frame.add(panel1);
 
-		table1.add(jsp);
-		frame.add(table1);
+		//Add label to panel
+		searchLabelPanel.add(searchLabel);
+		//Add textfield to panel
+		searchTextFieldPanel.add(searchTextField);
+		//Add panel to panel 
+		panel2.add(searchLabelPanel);
+		panel2.add(searchTextFieldPanel);
+		//Add panel to frame
+		frame.add(panel2);
 
+		//Add scrollable table to panel
+		tablePanel.add(scrollPane);
+		//Add panel to frame
+		frame.add(tablePanel);
+
+		//Set color of frame border
 		frame.getRootPane().setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.BLUE));
-		frame.setSize(1350, 800);
+		 //Set size of frame
+		frame.setSize(1350, 900);
+		//Set frame to be visible
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
-	private void loadCustomerData() {
-		allCustomers = new Controller().getAllCustomers();
 		
-		allCustomers.forEach(customer -> {
-			Vector<Object> items = new Vector<>();
+		private void loadCustomerData() {
+			allCustomers = new Controller().getAllCustomers();
 			
-			items.add(customer.getId());
-			items.add(customer.getName());
-			items.add(customer.getDob());
-			items.add(customer.getAddress());
-			items.add(customer.getTelephone());
-			items.add(customer.getEmail());
-			items.add(customer.getDateOfMembership());
-			items.add(customer.getDateOfMembershipExp());
+			allCustomers.forEach(customer -> {
+				Vector<Object> items = new Vector<>();
+				
+				items.add(customer.getId());
+				items.add(customer.getName());
+				items.add(customer.getDob());
+				items.add(customer.getAddress());
+				items.add(customer.getTelephone());
+				items.add(customer.getEmail());
+				items.add(customer.getDateOfMembership());
+				items.add(customer.getDateOfMembershipExp());
+				
+				tableItems.addRow(items);
+			});
 			
-			tableItems.addRow(items);
-		});
-		
-	}
+		}
 
+	
 	public static void main(String[] args) {
 
 		new CustomerDatabase();
